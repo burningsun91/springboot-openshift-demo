3ec00f96a5f9caa4934868523bcee21f64fd8bfe
Suraj <iamsurajmehta@gmail.com>
2020-04-13 16:12:47 +0530

    Init

--- src/main/java/com/example/openshift/controller/TestController.java
+++ src/main/java/com/example/openshift/controller/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 	
-	@GetMapping("/openshift/demo")
+	@GetMapping("/")
 	public String helloOpenShift() {
 		return "Welcome to Openshift";
 	}
